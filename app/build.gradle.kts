c18e23b8b15e52f902410f0c82963e770d478aed
snowsan0113 <egawayuuki0113@gmail.com>
2025-09-29 20:42:19 +0900

    【追加】ボタンを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "snowsan0113.calculator_android_app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "snowsan0113.calculator_android_app"
